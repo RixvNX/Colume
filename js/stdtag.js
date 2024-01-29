49f5fb956773eac895370e7a26178cfa82b78f72
RixvNX <142212639+RixvNX@users.noreply.github.com>
2024-01-29 20:27:50 +0800

    testversion_beta03

--- js/stdtag.js
+++ js/stdtag.js
@@ -1,22 +1,32 @@
-var stdtags_v1 = ["colume"];
+var stdtags_v1 = ["colume", "colime", "space", "code", "box", "text", "codesign", "value"];
+var overwitre_v1 = ["button"];
 
-function STDTag(tagName, fn){
-    Array
-      .from(document.getElementsByTagName(tagName))
-      .forEach(fn);
+function checkCodeTitleAndValue() {
+  if (document.getElementsByTagName("title").length != document.getElementsByTagName("value").length) {
+    console.log("[From stdtag] Checker: Missing Code_Title or Code_Value? lenght of AssemblyCodeTag is OOD!");
   }
-  
-  function columeHandler(element) {
-    console.log("Included Colume Tag: " + stdtags_v1[i] + "  ->  " + "Success");
-  } 
+}
 
+function STDTag(tagName){
+    Array
+      .from(document.getElementsByTagName(tagName));
+  }
 
-// Handle Tags
+// print to console
 for(var i=0; i<stdtags_v1.length; i++) {
-    STDTag(stdtags_v1[i], eval(stdtags_v1[i]+"Handler"));
+    STDTag(stdtags_v1[i]);
+    console.log("[From stdtag] Included Tags: " + stdtags_v1[i]);
+};
+for(var i=0; i<overwitre_v1.length; i++) {
+  console.log("[From stdtag] Overwite Tags: " + overwitre_v1[i]);
 };
 
 // Script
-function Button_onClick(actions) {
+function Button_onClick(actions, args) {
   console.log(actions)
+  if (actions == "$alert") {
+    alert(args)
+  }
 }
+
+checkCodeTitleAndValue()
